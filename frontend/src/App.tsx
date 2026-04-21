7182268f7eba265ef5542340460a69e66d5556fe
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-04-21 16:57:54 +0200

    add login button in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,14 @@ import './App.css'
 function App() {
 
 
+  function login(){
+    const host : string = window.location.host === "localhost:5173" ? "http://localhost:8080" : window.location.origin
+    window.open(host + "/oauth2/authorization/github" , "_self")
+  }
+
   return (
     <>
-
+      <button onClick={login}>Login</button>
     </>
   )
 }
