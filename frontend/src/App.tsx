e73341a46b4701b058a2717da6039ac68cc12963
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-04-23 16:15:38 +0200

    remove initial test buttons for login and logout add Route for logoutPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,9 @@ import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
 import AuthCallbackPage from "./pages/AuthCallbackPage.tsx";
-
-function login() {
-    const host: string =
-        globalThis.location.host === "localhost:5173"
-            ? "http://localhost:8080"
-            : globalThis.location.origin
-
-    globalThis.open(host + "/oauth2/authorization/github", "_self")
-}
-
-function logout() {
-    const host =
-        globalThis.location.host === "localhost:5173"
-            ? "http://localhost:8080"
-            : globalThis.location.origin
-
-    globalThis.open(host + "/logout", "_self")
-}
+import LogoutPage from "./pages/LogoutPage.tsx";
 
 function App() {
-
-
   return (
       <div className={"app"}>
         <Header />
@@ -33,9 +14,8 @@ function App() {
              <Routes>
                  <Route path="/register" element={<RegisterPage />} />
                  <Route path="/auth/callback" element={<AuthCallbackPage />} />
+                 <Route path="/logout" element={<LogoutPage />} />
              </Routes>
-          <button onClick={login}>Login</button>
-          <button onClick={logout}>Logout</button>
         </main>
         <Footer />
       </div>
