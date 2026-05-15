bde11a8e3aaa4ce9c087a0be7dc680f89e5ad4c2
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-05-15 16:06:46 +0200

    edit CheckoutRequest with @NotNull @Valid Annotations

--- backend/src/main/java/org/example/backend/order/dto/CheckoutRequest.java
+++ backend/src/main/java/org/example/backend/order/dto/CheckoutRequest.java
@@ -1,11 +1,23 @@
 package org.example.backend.order.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.example.backend.common.model.Address;
 import org.example.backend.order.model.OrderItem;
 
 public record CheckoutRequest(
+
+        @NotNull(message = "Items dürfen nicht null sein.")
+        @NotEmpty(message = "Der Warenkorb darf nicht leer sein.")
         OrderItem[] items,
+
+        @NotNull(message = "Die Lieferadresse ist erforderlich.")
+        @Valid
         Address shippingAddress,
+
+        @NotNull(message = "Die Rechnungsadresse ist erforderlich.")
+        @Valid
         Address billingAddress
 
 ) {
