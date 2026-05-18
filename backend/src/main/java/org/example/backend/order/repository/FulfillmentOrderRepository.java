3e0c55a785edefc9cb0fead70af361c176b55535
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-05-18 09:27:53 +0200

    add neccessary repo functions for services

--- backend/src/main/java/org/example/backend/order/repository/FulfillmentOrderRepository.java
+++ backend/src/main/java/org/example/backend/order/repository/FulfillmentOrderRepository.java
@@ -4,6 +4,9 @@ import org.example.backend.order.model.FulfillmentOrder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FulfillmentOrderRepository extends MongoRepository<FulfillmentOrder, String> {
+    List<FulfillmentOrder> findByCustomerId(String customerId);
 }
--- backend/src/main/java/org/example/backend/order/repository/SellerOrderRepository.java
+++ backend/src/main/java/org/example/backend/order/repository/SellerOrderRepository.java
@@ -4,6 +4,9 @@ import org.example.backend.order.model.SellerOrder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SellerOrderRepository extends MongoRepository<SellerOrder, String> {
+    List<SellerOrder> getSellerOrderBySellerId(String id);
 }
