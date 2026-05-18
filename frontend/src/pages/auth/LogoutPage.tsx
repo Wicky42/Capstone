1124c4a66862372fed01ed10e5cf7d78155cd99e
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-05-18 15:56:41 +0200

    edit Logout Page: if you logout, please logout in github as well

--- frontend/src/pages/auth/LogoutPage.tsx
+++ frontend/src/pages/auth/LogoutPage.tsx
@@ -6,6 +6,14 @@ export default function LogoutPage() {
             <div className="logoutpage__card">
                 <h2>Auf Wiedersehen!</h2>
                 <p>Du wurdest erfolgreich ausgeloggt.</p>
+                <a
+                    href="https://github.com/logout"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="button-primary"
+                >
+                    Auch bei GitHub abmelden (für Account-Wechsel)
+                </a>
             </div>
         </section>
     );
