8ba8afb3f89418124516aa972e3eaeff369a588f
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-05-18 17:09:29 +0200

    add OrderConfirmationPage to Routes

--- backend/src/main/java/org/example/backend/order/dto/CheckoutResponse.java
+++ backend/src/main/java/org/example/backend/order/dto/CheckoutResponse.java
@@ -2,15 +2,9 @@ package org.example.backend.order.dto;
 
 import org.example.backend.order.model.FulfillmentOrderStatus;
 
-//{
-//        "orderId": "fo-1001",
-//        "invoiceId": "inv-3001",
-//        "status": "CREATED",
-//        "totalPrice": 23.47
-//        }
 public record CheckoutResponse(
-        String orderId,
-        String invoiceId,
+        String orderNumber,
+        String invoiceNumber,
         FulfillmentOrderStatus status,
         double totalPrice
 ) {
