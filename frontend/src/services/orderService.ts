11e3dc72ced91cbc0b2e1f51f5ef3dcc8d693bd3
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-05-18 17:05:48 +0200

    edit orderService get<FulfillmentOrder>(`/customer/orders/${orderId}`

--- frontend/src/services/orderService.ts
+++ frontend/src/services/orderService.ts
@@ -1,8 +1,14 @@
 import api from "./api";
-import type { CheckoutRequest, CheckoutResponse } from "../types/order";
+import type { CheckoutRequest, CheckoutResponse, FulfillmentOrder } from "../types/order";
+
 export const orderService = {
     async checkout(request: CheckoutRequest): Promise<CheckoutResponse> {
         const response = await api.post<CheckoutResponse>("/orders/checkout", request);
+        return response.data;
+    },
+
+    async getOrderById(orderId: string): Promise<FulfillmentOrder> {
+        const response = await api.get<FulfillmentOrder>(`/customer/orders/${orderId}`);
         return response.data;
     },
 };
